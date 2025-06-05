768b6061c9bb0c8a05a3c2d13af74f12f82a071d
Diego DPL <168758994+Diego-DPL@users.noreply.github.com>
2025-06-05 14:51:08 +0200

    Add Tailwind config and restore h-150

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      height: {
+        150: '37.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
